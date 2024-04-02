0f76c40169040c26573ede31c033ec58381c273f
Binny Chopra <binnyc@umd.edu>
2024-04-02 11:22:27 -0400

    Ep-03

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {}, "Hello from React!");
+const title = <h1>Title elm</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+const HeadingComponent = () => (
+  <div className="container">
+    {title}
+    <h2>Namaste</h2>
+  </div>
+);
+root.render(<HeadingComponent />);
